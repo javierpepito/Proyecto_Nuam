fdf98282dd54cd8dc9a035e6110931afb3be3619
javierpepito <javiermendezsepulveda19@gmail.com>
2025-11-21 17:55:20 -0300

    Feat: Creacion de template

--- Contenedor_Calificaciones/urls.py
+++ Contenedor_Calificaciones/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.base, name="base"),
+    path("", views.Inicio_Calificador, name="Inicio_Calificador"),
+    #path("calificador/inicio/", views.Inicio_Calificador, name="Inicio_Calificador"),
 ]
--- Contenedor_Calificaciones/views.py
+++ Contenedor_Calificaciones/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-#Vista para ver html base
-def base(request):
-    return render(request, 'Contenedor_Calificaciones/base.html')
+#Vista inicial de calificador tributario
+def Inicio_Calificador(request):
+    return render(request, 'Contenedor_Calificaciones/Inicio_Calificador.html')
